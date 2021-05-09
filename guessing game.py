711a315da96b2132593b20ccc328245beaf0ed3a
Kweezi Perez Christopher <kweeziperez712@gmail.com>
2021-05-10 01:01:05 +0300

    Update guessing game.py

--- guessing game.py
+++ guessing game.py
@@ -12,19 +12,24 @@ print("Hello", username +".", )
 question = input("Would you like to play a game? [Y/N] ")
 if question == "n":
     print("oh ..okay")
+    
 if question =="y":
     print("I'm thinking of a number between 1 & 10")
     guess = int(input("Have a guess: "))
+    
     if guess > number:
         print("Guess lower...")
+        
 if guess < number:
     print("Guess higher...")
+    
 while guess != number:
     tries += 1
     guess = int(input("Try again: "))
+    
     if guess < number:
         print("You're right! you win! The number was", number, \
-            "and it only", tries, "tries!")
+            "and it only took you", tries, "tries!")
      
 
 
